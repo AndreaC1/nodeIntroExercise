a9dce74b8d52ca48664181f5e6cfb581b4985851
Andrea Cseri <andrea.cseri27@gmail.com>
2024-07-15 08:27:27 -0700

    completed exercise

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+    response.end("hello")
+});
+
+server.listen(5555, () => {
+    console.log("Server started on port 5555..")
+})
